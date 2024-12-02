5fe9314ad3c25fc0dc323fc9ae7416608a79fa83
Rutuja <rutujarathod50@gmail.com>
2024-12-02 14:35:41 +0530

    print the value of a and b

--- Test/src/com/velocity/Example.java
+++ Test/src/com/velocity/Example.java
@@ -6,6 +6,8 @@ public class Example {
 		int a = 80;
 		int b = 50;
 		int c = a - b;
+		System.out.println("Value of a >> " + a);
+		System.out.println("Value of b >> " + b);
 		System.out.println("Subtraction is >> " + c);
 
 	}
