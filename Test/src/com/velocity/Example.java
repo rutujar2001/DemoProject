74fdec2c8c0292eae79ac8cd45e47ea13b93dd2b
Rutuja <rutujarathod50@gmail.com>
2024-12-02 14:11:31 +0530

    subtraction code

---
+++ Test/src/com/velocity/Example.java
@@ -0,0 +1,12 @@
+package com.velocity;
+
+public class Example {
+
+	public static void main(String[] args) {
+		int a = 80;
+		int b = 50;
+		int c = a - b;
+		System.out.println("Subtraction is >> " + c);
+
+	}
+}
